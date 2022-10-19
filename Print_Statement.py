75848a195005db971badf9c728a6ceedc20871ec
sandykri09 <115703445+sandykri09@users.noreply.github.com>
2022-10-19 12:48:06 +0530

    Add files via upload

---
+++ Print_Statement.py
@@ -0,0 +1,32 @@
+# print('hello world')
+# print("hello world")
+# print('''hello world''')
+# print("""hello world""")
+
+# print('hello wo\'rld')
+# print("hello wo'rld")
+# print('''hello wo'rld''')
+# print("""hello wo'rld""")
+
+# print('print(hello world)')
+# print("print(hello world)")
+# print('''print(hello world)''')
+# print("""print(hello world)""")
+
+# print('print(hello wor\'ld)')
+# print("print(hello wor'ld)")
+# print('''print(hello wor'ld)''')
+# print("""print(hello wor'ld)""")
+
+print('hello we have started coding in python we are \
+doing the print statement topic on tuesday.')
+
+print("hello we have started coding in python we are \
+doing the print statement topic on tuesday.")
+
+print('''hello we have started coding in python we are \
+doing the print statement topic on tuesday.''')
+
+print("""hello we have started coding in python we are \
+doing the print statement topic on tuesday.""")
+
